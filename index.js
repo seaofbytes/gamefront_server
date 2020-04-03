898b2f8a7d7b2eb2bb0517b4003b6b2ff9cf9c27
seaofbytes <sandilukez1990@gmail.com>
2020-04-03 12:26:15 +0200

    Added AdminBro functionality

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ const port = process.env.PORT || 4000
 const corsMiddleware = cors()
 const bodyParser = require('body-parser')
 const parser = bodyParser()
+const categoryRoutes = require('./routes/category')
 const db = require('./db')
 
-
+// Routers
+const userRoutes = require('./routes/user')
+const productRoutes = require('./routes/product')
+const cartRoutes = require('./routes/cart')
+const reviewRoutes = require('./routes/review')
+const checkoutRoutes = require('./routes/checkout')
+const orderRoutes = require('./routes/order')
+const newAdminBro = require('./admin/admin').newAdminBro
+const adminRouter = require('./admin/admin').adminRouter
 
 // Middleware
 app.use(corsMiddleware)
 app.use(parser)
 
-
+// Routes
+app.use(userRoutes)
+app.use(productRoutes)
+app.use(categoryRoutes)
+app.use(cartRoutes)
+app.use(reviewRoutes)
+app.use(checkoutRoutes)
+app.use(orderRoutes)
+app.use(newAdminBro.options.rootPath, adminRouter)
 app.get('/', (req, res) => {
   res.send('Hello world !')
 })
